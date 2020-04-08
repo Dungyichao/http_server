6f82446b956b1153859142241dca577b5982a0fb
Dung-Yi <dungyichao@gmail.com>
2020-04-08 07:42:10 -0400

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,140 @@
+/*
+ * main.c
+ * 
+ * Copyright 2020  <pi@raspberrypi>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+// Server side C program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+#define PORT 8081
+void setHttpHeader(char httpHeader[]);
+void report(struct sockaddr_in *serverAddress);
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    char httpHeader[100000] = "HTTP/1.1 200 OK\r\n\n";
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In sockets");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    //report(&address);
+    setHttpHeader(httpHeader);
+    
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , httpHeader , strlen(httpHeader));
+        printf("------------------Hello message sent-------------------\n");
+        close(new_socket);
+    }
+    return 0;
+}
+
+void setHttpHeader(char httpHeader[])
+{
+    
+    // File object to return
+    
+    FILE *htmlData = fopen("index.html", "r");
+
+
+    char line[100];
+    char responseData[1000000];
+    if(htmlData){
+        while (fgets(line, 100, htmlData) != 0 ) {
+            strcat(responseData, line);
+        }
+        // char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
+        strcat(httpHeader, responseData);
+        fclose(htmlData);
+    }
+    else
+    {
+        printf("Read file problem");
+    }
+}
+
+void report(struct sockaddr_in *serverAddress)
+{
+    char hostBuffer[INET6_ADDRSTRLEN];
+    char serviceBuffer[NI_MAXSERV]; // defined in `<netdb.h>`
+    socklen_t addr_len = sizeof(*serverAddress);
+    int err = getnameinfo(
+        (struct sockaddr *) serverAddress,
+        addr_len,
+        hostBuffer,
+        sizeof(hostBuffer),
+        serviceBuffer,
+        sizeof(serviceBuffer),
+        NI_NUMERICHOST
+    );
+    if (err != 0) {
+        printf("It's not working!!\n");
+    }
+    printf("\n\n\tServer listening on http://%s:%s\n", hostBuffer, serviceBuffer);
+}
