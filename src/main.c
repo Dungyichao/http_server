942d49994944c1db3c0c0ae4bd251cebb48fa56f
Dung-Yi <dungyichao@gmail.com>
2020-04-08 22:38:42 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -33,9 +33,11 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 
-#define PORT 8081
+#define PORT 8080
 void setHttpHeader(char httpHeader[]);
+void setHttpHeader_other(char httpHeader[], char *path);
 void report(struct sockaddr_in *serverAddress);
+char* parse(char line[]);
 
 int main(int argc, char const *argv[])
 {
@@ -44,6 +46,7 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     
     char httpHeader[100000] = "HTTP/1.1 200 OK\r\n\n";
+    char httpHeader1[3800000] = "HTTP/1.1 200 OK\r\n\n";
     
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -87,10 +90,25 @@ int main(int argc, char const *argv[])
         
         char buffer[30000] = {0};
         valread = read( new_socket , buffer, 30000);
-        printf("%s\n",buffer );
-        write(new_socket , httpHeader , strlen(httpHeader));
-        printf("------------------Hello message sent-------------------\n");
+        
+        //printf("%s\n",buffer );
+        
+        char *parse_string = parse(buffer);
+        printf("path: %s\n", parse_string);
+        if(strlen(parse_string) <= 1){
+            write(new_socket , httpHeader , strlen(httpHeader));
+            printf("\n Send index.html file \n");
+        }
+        else{
+            setHttpHeader_other(httpHeader1, parse_string);
+            write(new_socket , httpHeader1 , strlen(httpHeader1));
+            printf("\n Send other file, size: %d \n", strlen(httpHeader1));
+            printf("%s", httpHeader1);
+        }
+        //printf("This is the pathe: %s", parse_path);
+        printf("------------------Server sent-------------------\n");
         close(new_socket);
+        //free(parse_string);
     }
     return 0;
 }
@@ -100,7 +118,8 @@ void setHttpHeader(char httpHeader[])
     
     // File object to return
     
-    FILE *htmlData = fopen("index.html", "r");
+    FILE *htmlData = fopen("inde.html", "r");
+    //FILE *htmlData = fopen("./img/inde.html", "r");
 
 
     char line[100];
@@ -115,7 +134,34 @@ void setHttpHeader(char httpHeader[])
     }
     else
     {
-        printf("Read file problem");
+        printf("\n Read index.html file problem");
+    }
+}
+
+void setHttpHeader_other(char httpHeader[], char *path)
+{
+    
+    // File object to return
+    
+    char path_head[500] = ".";
+    strcat(path_head, path);
+    printf("\n path head : %s", path_head);
+    
+    FILE *htmlData1 = fopen(path_head, "r");
+
+    ////char httpHeader1[8000] = "HTTP/1.1 200 OK\r\n\n";
+    char line[100];
+    char responseData[3800000];
+    if(htmlData1){
+        while (fgets(line, 100, htmlData1) != 0 ) {
+            strcat(responseData, "TTT");
+        }
+        strcat(httpHeader, responseData);
+        fclose(htmlData1);
+    }
+    else
+    {
+        printf("\n Read other file problem");
     }
 }
 
@@ -138,3 +184,30 @@ void report(struct sockaddr_in *serverAddress)
     }
     printf("\n\n\tServer listening on http://%s:%s\n", hostBuffer, serviceBuffer);
 }
+
+char* parse(char line[])
+{
+    //char *path;
+    char *message;
+    char * token = strtok(line, " ");
+    int current = 0;
+    
+    message = "No path";
+    printf("token--------------------\n");
+    while( token != NULL ) {
+      if(current < 2){
+          printf( " %s\n", token ); //printing each token
+      }
+      
+      token = strtok(NULL, " ");
+      if(current == 0){
+          message = token;
+          return message;
+      }
+      current = current + 1;
+      
+   }
+   return message;
+    
+}
+
